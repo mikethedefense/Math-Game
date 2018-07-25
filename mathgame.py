c0ebe275146e88ac7d0793f53e5f3eff085a7851
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2018-07-24 21:25:56 -0400

    Create mathgame.py

---
+++ mathgame.py
@@ -0,0 +1,28 @@
+# To-do list: 
+# https://stackoverflow.com/questions/15528939/python-3-timed-input
+# Add timed input (For later)
+import random
+proceed = ""
+score = 0
+while proceed == "y" or proceed == "":
+    try:
+        count = random.randint(1,12)
+        times_value = random.randint(1,12) 
+        result = times_value * count
+        print('What is {} times {}:'.format(times_value,count))
+        number = input('Please enter a number:')
+        number = int(number)
+    except ValueError:
+        print('Invalid text. Please enter correct digits')
+    
+    
+    if number == result:
+        print('You are correct')
+        score = score +1
+    else:
+        print('You are incorrect')
+        print('The answer was {}'.format(result))
+
+    print('Your current score is {}'.format(score))
+    
+    proceed = input("Play again? Y/n:")
